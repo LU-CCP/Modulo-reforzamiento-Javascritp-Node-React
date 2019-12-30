0e61265ffeffeb216f1702e8bc3ae4258a154f35
leonardop-lagash <56941459+leonardop-lagash@users.noreply.github.com>
2019-12-30 16:23:27 -0300

    instalamos nodemon y mongoose

---
+++ Node/leonardo-pereira/API/routes/students/index.js
@@ -0,0 +1,11 @@
+const Router = require("restify-router").Router;
+
+const router = new Router();
+
+// url base/v1/students
+
+router.get("/v1/students", function(req, res) {
+  res.send(200, "Hello from students");
+});
+
+module.exports = router;
--- Node/leonardo-pereira/API/server.js
+++ Node/leonardo-pereira/API/server.js
@@ -1,9 +1,17 @@
 const restify = require("restify");
 const server = restify.createServer();
+const Router = require("restify-router").Router;
 
-server.get("api/status", function(req, res) {
+const router = new Router();
+const studentsRoutes = require("./routes/students");
+
+server.get("/api/status", function(req, res) {
   res.send(200, "API is alive!");
 });
+
+router.add("", studentsRoutes);
+router.applyRoutes(server);
+
 server.listen(8080, function() {
-  console.log("servidor corriendo en el puerto : 8080");
+  console.log("servidor corriendo en el puerto : ", 8080);
 });
