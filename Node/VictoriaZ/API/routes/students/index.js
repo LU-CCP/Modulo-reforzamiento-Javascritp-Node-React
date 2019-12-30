473e332295b9f0439c2192aac98e6c192f47a792
Victoria Zanforlin <victoriaz@lagash.com>
2019-12-30 16:43:58 -0300

    se instala mongoose y agrega petición get para obtener información de students

--- Node/VictoriaZ/API/routes/students/index.js
+++ Node/VictoriaZ/API/routes/students/index.js
@@ -1,9 +1,21 @@
 const Router = require("restify-router").Router;
+const mongoose = require("mongoose");
+const Schema = require("mongoose").Schema;
+const studentSchema = new Schema({
+  name: String,
+  surname: String
+});
 
 const router = new Router();
-
+const userModel = mongoose.model("students", studentSchema);
 //url base/v1/students
 router.get("/v1/students", function(req, res) {
+  userModel.find({}, function(err, result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log("students", result);
+  });
   res.send(200, "Hello from students");
 });
 
--- Node/VictoriaZ/API/server.js
+++ Node/VictoriaZ/API/server.js
@@ -4,6 +4,7 @@ const router = new Router();
 
 const studentsRoutes = require("./routes/students");
 
+const mongoose = require("mongoose");
 const server = restify.createServer();
 server.get("/api/status", function(req, res) {
   res.send(200, "Api is alive!");
@@ -11,6 +12,10 @@ server.get("/api/status", function(req, res) {
 
 router.add("", studentsRoutes);
 router.applyRoutes(server);
-server.listen(8080, function() {
+server.listen(8080, async function() {
+  await mongoose.connect("mongodb://localhost/DBej", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
   console.log("server running on port 8080: ", 8080);
 });
