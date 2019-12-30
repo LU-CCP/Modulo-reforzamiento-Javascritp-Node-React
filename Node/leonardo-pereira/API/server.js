d4fe85cf435f8fbd605b4c30f1f29a23b0ff2c73
leonardop-lagash <56941459+leonardop-lagash@users.noreply.github.com>
2019-12-30 15:32:22 -0300

    se creo una petiticion de tipo get, para el status

--- Node/leonardo-pereira/API/server.js
+++ Node/leonardo-pereira/API/server.js
@@ -1,6 +1,9 @@
 const restify = require("restify");
 const server = restify.createServer();
 
+server.get("api/status", function(req, res) {
+  res.send(200, "API is alive!");
+});
 server.listen(8080, function() {
   console.log("servidor corriendo en el puerto : 8080");
 });
