8955180106758b66d225cf9c3b7f90579bf1fbcb
leonardop-lagash <56941459+leonardop-lagash@users.noreply.github.com>
2019-12-30 16:45:22 -0300

    se realizó la coneccion con nuestra base de datos leoDB (mongoDB)

--- Node/leonardo-pereira/API/routes/students/index.js
+++ Node/leonardo-pereira/API/routes/students/index.js
@@ -1,10 +1,24 @@
 const Router = require("restify-router").Router;
+const mongoose = require("mongoose");
+const Schema = require("mongoose").Schema;
+
+const studentsSchema = new Schema({
+  name: String,
+  surname: String
+});
 
 const router = new Router();
+const userModel = mongoose.model("students", studentsSchema);
 
 // url base/v1/students
 
 router.get("/v1/students", function(req, res) {
+  userModel.find({}, function(err, result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log("students", result);
+  });
   res.send(200, "Hello from students");
 });
 
--- Node/leonardo-pereira/API/server.js
+++ Node/leonardo-pereira/API/server.js
@@ -1,6 +1,7 @@
 const restify = require("restify");
 const server = restify.createServer();
 const Router = require("restify-router").Router;
+const mongoose = require("mongoose");
 
 const router = new Router();
 const studentsRoutes = require("./routes/students");
@@ -12,6 +13,11 @@ server.get("/api/status", function(req, res) {
 router.add("", studentsRoutes);
 router.applyRoutes(server);
 
-server.listen(8080, function() {
+server.listen(8080, async function() {
+  await mongoose.connect("mongodb://localhost/leoDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
   console.log("servidor corriendo en el puerto : ", 8080);
 });
