d106764f22102c2016db250c93de318d41ad371d
bryanm-lagash <bryanm@lagash.com>
2019-12-31 20:29:29 -0300

    Ultimos cambios realizados

--- Node/BryanM/api/routes/students/index.js
+++ Node/BryanM/api/routes/students/index.js
@@ -1,9 +1,62 @@
 const Router = require("restify-router").Router;
-
+const mongoose = require("mongoose");
+const Schema = require("mongoose").Schema;
+const studentSchema = new Schema({
+  name: String,
+  surname: String
+});
 const router = new Router();
+const useModel = mongoose.model("students", studentSchema);
 
 router.get("/v1/students", function(request, response) {
-  response.send(200, "Hello from students");
+  console.log(request.body);
+  useModel.find({}, function(err, result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log("students", result);
+    response.send(result);
+  });
+});
+
+//AGREGAR
+router.post("/v1/students/", async function(request, response) {
+  const { name, surname } = request.body;
+  let student = new useModel({ name, surname });
+  let results;
+
+  await student.save(function(error, student) {
+    if (error) {
+      console.log(error);
+    }
+    results = student;
+  });
+
+  response.send(200, "Agregado correctamente!");
+});
+
+router.del("/v1/students/:name", async function(request, response) {
+  await useModel.deleteOne({ name: request.params.name }),
+    function(err) {
+      if (err) console.log(err);
+    };
+  response.send("Eliminado correctamente");
+});
+
+router.put("/v1/students/:id", async (request, response) => {
+  const { Name, Surname } = request.body;
+  console.log(request.body);
+  console.log(request.params.id);
+  await useModel.updateOne(
+    { id: request.params.id },
+    { name: Name, surname: Surname },
+    function(err, res) {
+      if (err) {
+        console.log(err);
+      }
+      response.send("Actualizado correctamente", res);
+    }
+  );
 });
 
 module.exports = router;
--- Node/BryanM/api/server.js
+++ Node/BryanM/api/server.js
@@ -2,15 +2,22 @@ const restify = require("restify");
 const Router = require("restify-router").Router;
 const router = new Router();
 const studentsRoutes = require("./routes/students");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const server = restify.createServer();
 server.get("/api/status", function(request, response) {
   response.send(200, "Api is alive!");
 });
 
+server.use(bodyParser.json());
 router.add("", studentsRoutes);
 router.applyRoutes(server);
 
-server.listen(3000, function() {
+server.listen(3000, async function() {
+  await mongoose.connect("mongodb://localhost/dbPatricio", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
   console.log("Server running on port:", 3000);
 });
