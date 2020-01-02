10be4b0a0b628dbe031eef816b2b5bd5a6ffa125
melinag <melinag@lagash.com>
2020-01-02 17:03:57 -0300

    metodo post de admin funcional

--- Node/melinaG/Api/routes/admin/index.js
+++ Node/melinaG/Api/routes/admin/index.js
@@ -8,19 +8,32 @@ const userSchema = new Schema({
 const userModel = mongoose.model("admins", userSchema);
 const router = new Router();
 
-router.post("/v1/admin/", async function(req, res) {
-  console.log(req.body);
+router.post("/v1/admin/post", async function(req, res) {
+  console.log("inicio");
+  let data;
   const n = req.body;
+  await userModel.findOne(n, function(err, result) {
+    if (err) res.send(401, "error al buscar");
+    data = result;
+  });
+
+  if (data) {
+    res.send(200, "usuario permitido");
+  } else {
+    res.send(401, "usuario no permitido");
+  }
+});
+
+router.get("/v1/admins", async function(req, res) {
+  let admins;
   await userModel.find({}, function(err, result) {
     if (err) {
       console.log(err);
     }
-    console.log(result);
-    if (n === result) {
-      res.send(200, "usuario permitido");
-    } else {
-      res.send(401, "usuario no permitido");
-    }
+    admins = result;
+    console.log("students", result);
   });
+  res.send(200, admins);
 });
+
 module.exports = router;
