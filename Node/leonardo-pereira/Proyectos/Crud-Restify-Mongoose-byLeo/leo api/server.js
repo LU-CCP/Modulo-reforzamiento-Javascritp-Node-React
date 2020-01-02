31d01e9bb215c450587f6436b0ca56fa847ccc75
leonardop-lagash <56941459+leonardop-lagash@users.noreply.github.com>
2020-01-02 17:40:41 -0300

    clase 02_01_20

--- Node/leonardo-pereira/Proyectos/Crud-Restify-Mongoose-byLeo/leo api/server.js
+++ Node/leonardo-pereira/Proyectos/Crud-Restify-Mongoose-byLeo/leo api/server.js
@@ -3,13 +3,37 @@ const Router = require("restify-router").Router;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const studentRutas = require("./rutas");
+const login = require("./rutas/admin");
+////
+const jwt = require("jsonwebtoken");
+const rjwt = require("restify-jwt-community");
 
 //creando servidor
 
 const miServer = restify.createServer();
+miServer.use(
+  rjwt({ secret: "my-secret-key" }).unless({
+    path: ["/login"]
+  })
+);
+
+miServer.use(function(req, res, next) {
+  if (req.url === "/login") {
+    return next();
+  }
+  let authorization = req.header("authorization").split(" ");
+  try {
+    var decoded = jwt.verify(authorization[1], "my-secret-key");
+    return next();
+  } catch (error) {
+    res.send(401, { message: "no autorizado" });
+    return next();
+  }
+});
 const rutas = new Router();
 
 rutas.add("", studentRutas);
+rutas.add("", login);
 rutas.applyRoutes(miServer);
 
 miServer.use(bodyParser.json());
