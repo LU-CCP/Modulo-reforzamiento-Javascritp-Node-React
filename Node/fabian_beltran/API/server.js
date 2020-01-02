245e17185d4bb39d25f01d1c3b763071132a60ff
fabianb-lagash <fabianb@lagash.com>
2020-01-02 18:02:49 -0300

    Se modifica y agregan lineas de codigo al proyecto API

--- Node/fabian_beltran/API/server.js
+++ Node/fabian_beltran/API/server.js
@@ -2,13 +2,15 @@ const restify = require("restify");
 const Router = require("restify-router").Router;
 
 const router = new Router();
-const studentsRoutes = require("./routes/students");
+const studentsRoutes = require("./routes/admin");
 const mongoose = require("mongoose");
 
+const rjwt = require("restify-jwt-community");
+const jwt = require("jsonwebtoken");
+
 const server = restify.createServer();
-server.get("/api/status", function(req, res) {
-  res.send(200, "Api is alive!!!!!");
-});
+server.use(restify.plugins.jsonBodyParser());
+server.use(rjwt({ secret: "my-secret-key" }).unless({ path: ["/v1/admin"] }));
 
 router.add("", studentsRoutes);
 router.applyRoutes(server);
@@ -20,5 +22,3 @@ server.listen(8080, async function() {
   });
   console.log("server running on port: ", 8080);
 });
-
-
