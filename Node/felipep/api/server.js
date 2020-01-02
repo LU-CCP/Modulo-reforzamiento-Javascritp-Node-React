57328143d9ff80e43d3aea65f582e15445a74910
felipep-lagash <felipep@lagash.com>
2020-01-02 00:00:20 -0300

    Se completó CRUD

--- Node/felipep/api/server.js
+++ Node/felipep/api/server.js
@@ -1,7 +1,26 @@
 const restify = require('restify');
+const Router = require("restify-router").Router;
+const router = new Router();
+const studentsRoutes = require('./routes/students');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
 
-const server = restify.createServer();
 
-server.listen(8080, () => {
+const server = restify.createServer()
+
+server.use(bodyParser.json());
+
+server.get('/api/status', (req, res) => {
+    res.send(200, 'Api is alive!');
+})
+
+router.add('', studentsRoutes);
+router.applyRoutes(server);
+
+server.listen(8080, async () => {
+    await mongoose.connect('mongodb://localhost/db_students', {
+        userNewUrlParser: true,
+        useUnifiedTopology: true
+    });
     console.log('Server running on port', 8080);
 })
