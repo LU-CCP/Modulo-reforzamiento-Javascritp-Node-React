13dbf6c7fa0438aab579b7b977727585c76a55e1
mathiasm-lagash <56935943+mathiasm1@users.noreply.github.com>
2020-01-02 14:49:49 -0300

    ejercicio api

---
+++ Node/MathiasM/Api/server.js
@@ -0,0 +1,24 @@
+const restify = require("restify");
+const Router = require("restify-router").Router;
+const router = new Router();
+const studentsRouter = require("./routes/students");
+const mongoose = require("mongoose");
+
+const server = restify.createServer();
+
+const bodyParser = require("body-parser");
+server.use(bodyParser.json());
+
+server.get("/api/status", function(req, res) {
+  res.send(200, "Api is alive!");
+});
+
+router.add("", studentsRouter);
+router.applyRoutes(server);
+server.listen(8080, async function() {
+  await mongoose.connect("mongodb://localhost/LUDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  console.log("server running on port: ", 8080);
+});
