44be26f6483c1faad84dcb61269f0043249b34e9
marcoac001 <marcoac001@lagash.com>
2020-01-02 17:58:54 -0300

    SE HACE COMMIT DE LO TRABAJADO (JUNTO A FABIAN)

--- Node/main/routes/index.js
+++ Node/main/routes/index.js
@@ -2,5 +2,6 @@ const Router = require("restify-router").Router;
 const router = new Router();
 
 router.add("/v1", require("./students"));
+router.add("/v1/admins", require("./admins"));
 
 module.exports = router;
