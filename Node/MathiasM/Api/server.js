5ca42a46c221506cd47692034915e0a83d194108
mathiasm-lagash <56935943+mathiasm1@users.noreply.github.com>
2020-01-02 17:59:02 -0300

    agregado login con token

--- Node/MathiasM/Api/server.js
+++ Node/MathiasM/Api/server.js
@@ -2,9 +2,31 @@ const restify = require("restify");
 const Router = require("restify-router").Router;
 const router = new Router();
 const studentsRouter = require("./routes/students");
+const adminRouter = require("./routes/admin");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const rjwt = require("restify-jwt-community");
 
 const server = restify.createServer();
+server.use(restify.plugins.jsonBodyParser());
+server.use(
+  rjwt({ secret: "my-secret-key" }).unless({
+    path: ["/v1/login"]
+  })
+);
+server.use(function(req, res, next) {
+  if (req.url === "/v1/login") {
+    return next();
+  }
+  let authorization = req.header("authorization").split(" ");
+  try {
+    var decoded = jwt.verify(authorization[1], "my-secret-key");
+    return next();
+  } catch (err) {
+    res.send(401, { message: "Not authorized" });
+    return next();
+  }
+});
 
 const bodyParser = require("body-parser");
 server.use(bodyParser.json());
@@ -14,6 +36,7 @@ server.get("/api/status", function(req, res) {
 });
 
 router.add("", studentsRouter);
+router.add("", adminRouter);
 router.applyRoutes(server);
 server.listen(8080, async function() {
   await mongoose.connect("mongodb://localhost/LUDB", {
